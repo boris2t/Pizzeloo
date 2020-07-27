8cdc2d62ea2af36e252aa24921db8f70d3799b90
Boris Tityukov <boristityukov@gmail.com>
2020-07-27 15:57:19 +0300

    feat: Implement signUp and signIn pages

--- src/components/pages/signup/index.js
+++ src/components/pages/signup/index.js
@@ -2,11 +2,18 @@ import React, { useCallback } from 'react';
 import { withRouter } from 'react-router';
 import fire from "../../../fire";
 import Layout from '../../layout';
+import styles from './index.module.css'
 
 const SignUp = ({ history }) => {
   const handleSignUp = useCallback(async event => {
     event.preventDefault();
-    const { email, password } = event.target.elements;
+    const { email, password, rePassword } = event.target.elements;
+
+    if (password.value !== rePassword.value) {
+      console.log('Passwords do not match!')
+      return
+    }
+
     try {
       await fire
         .auth()
@@ -19,18 +26,24 @@ const SignUp = ({ history }) => {
 
   return (
     <Layout>
-      <h1>Sign up</h1>
-      <form onSubmit={handleSignUp}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Sign Up</button>
-      </form>
+      <div className={styles["signup-wrapper"]}>
+        <form onSubmit={handleSignUp} className={styles.form}>
+          <h2>Sign Up</h2>
+          <div className={styles["input-group"]}>
+            <input type="text" name="email" id="email" required />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className={styles["input-group"]}>
+            <input type="password" name="password" id="password" required />
+            <label htmlFor="password">Password</label>
+          </div>
+          <div className={styles["input-group"]}>
+            <input type="password" name="rePassword" id="re-password" required />
+            <label htmlFor="re-password">Confirm Password</label>
+          </div>
+          <input type="submit" value="Sign Up" className={styles["submit-btn"]} />
+        </form>
+      </div>
     </Layout>
   );
 };
